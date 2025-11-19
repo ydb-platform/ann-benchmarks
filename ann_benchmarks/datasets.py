b71a88e5c8f81fbde27d21e6e468708093df5b31
Evgeniy Ivanov <i@eivanov.com>
2025-11-19 15:54:19 +0100

    Add cohere-wikipedia-22-12-10M-angular-50K

--- ann_benchmarks/datasets.py
+++ ann_benchmarks/datasets.py
@@ -692,11 +692,14 @@ DATASETS: Dict[str, Callable[[str], None]] = {
 
     "cohere-wikipedia-22-12-100M-euclidean": lambda out_fn: cohere_wikipedia_22_12(out_fn, 100_000_000, 10_000, "euclidean"),
 
+    # 50K requests
+    "cohere-wikipedia-22-12-10M-angular-50K": lambda out_fn: cohere_wikipedia_22_12(out_fn, 10_000_000, 50_000, "angular"),
 
     # below are cohere-wikipedia-22-12 datasets with 1M requests
 
     "cohere-wikipedia-22-12-10k-angular-1M": lambda out_fn: cohere_wikipedia_22_12(out_fn, 10_000, 1_000_000, "angular"),
     "cohere-wikipedia-22-12-100M-angular-1M": lambda out_fn: cohere_wikipedia_22_12(out_fn, 100_000_000, 1_000_000, "angular"),
+
 }
 
 DATASETS.update({
